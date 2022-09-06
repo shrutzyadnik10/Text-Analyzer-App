9a400ea0030372c2579fdf319c6a11da9579a2de
shruti <sryadnik10@gmail.com>
2022-09-06 12:20:44 +0530

    Updates

---
+++ precache-manifest.8779449d0e0afb156c6f03fee0dc0deb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a9ed1173372c19ebc92",
+    "url": "/Text-Analyzer-App/static/css/main.7cea9ccc.chunk.css"
+  },
+  {
+    "revision": "6a9ed1173372c19ebc92",
+    "url": "/Text-Analyzer-App/static/js/main.6a9ed117.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/Text-Analyzer-App/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/Text-Analyzer-App/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "afc4b58f9cf2cf3e257c",
+    "url": "/Text-Analyzer-App/static/js/runtime~main.afc4b58f.js"
+  },
+  {
+    "revision": "5e1e100dda04dc68ba82b6b2a45dd837",
+    "url": "/Text-Analyzer-App/index.html"
+  }
+];
